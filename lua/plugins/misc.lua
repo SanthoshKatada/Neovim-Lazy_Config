ad614601a5bfde915225e7bbfc3716f960f4358e
SanthoshKatada <santhoshchampkatada@gmail.com>
2025-01-21 08:34:14 +0530

    Added something

--- lua/plugins/misc.lua
+++ lua/plugins/misc.lua
@@ -27,6 +27,31 @@ return {
 		opts = {},
 	},
 	{
+		-- Autoclose parentheses, brackets, quotes, etc.
+		"windwp/nvim-ts-autotag",
+		config = function()
+			require("nvim-ts-autotag").setup({
+				opts = {
+					-- Defaults
+					enable_close = true, -- Auto close tags
+					enable_rename = true, -- Auto rename pairs of tags
+					enable_close_on_slash = false, -- Auto close on trailing </
+				},
+				-- Also override individual filetype configs, these take priority.
+				-- Empty by default, useful if one of the "opts" global settings
+				-- doesn't work well in a specific filetype
+				per_filetype = {
+					["html"] = {
+						enable_close = false,
+					},
+				},
+			})
+		end,
+		event = "InsertEnter",
+		config = true,
+	},
+
+	{
 		"MeanderingProgrammer/render-markdown.nvim",
 		dependencies = { "nvim-treesitter/nvim-treesitter", "echasnovski/mini.nvim" }, -- if you use the mini.nvim suite
 		-- dependencies = { 'nvim-treesitter/nvim-treesitter', 'echasnovski/mini.icons' }, -- if you use standalone mini plugins
